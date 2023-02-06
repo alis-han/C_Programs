28709a175f147989ad07ce5b0a098fa5b5cb341e
alis-han <114800068+alis-han@users.noreply.github.com>
2023-02-06 09:56:18 +0530

    Add

---
+++ arthmetic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int a=10,b=6;
+    printf("%d",a+b);
+    printf("\n%d",a-b);
+    printf("\n%d",a*b);
+    printf("\n%d",a/b);
+    printf("\n%d",a%b);
+}
+
+// In modulo operator the sign depends on dividened whereas in normal division , maths rules.
